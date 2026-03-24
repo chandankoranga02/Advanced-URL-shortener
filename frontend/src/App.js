79523b70ff83d5618c4e54cce633d946bac2b859
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-03-24 21:18:22 +0530

    Setup config and all things completed

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Tailwind Working 🚀</h1>
+    </div>
+  );
+}
+
+export default App;
