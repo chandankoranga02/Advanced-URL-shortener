22659b8cc77b096aa5c6093017d76cf0b551c6b5
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-04-09 23:45:15 +0530

    core feature developed

--- Backend/controllers/Response_API.js
+++ Backend/controllers/Response_API.js
@@ -1,44 +1,47 @@
+const { nanoid } = require("nanoid");
 const bcrypt = require('bcrypt')
-const LInksData = require('../models/LinksData');
-
-
-const generateRandomId = () => {
-    return Math.random().toString(36).substring(2, 7);
-};
+const LinksData = require('../models/LinksData');
+const mongoose =  require('mongoose')
 
 
 
 exports.Response_POST_API = async (req, res) => {
 
 
-    const { originalURL, password, expiryDate } = req.body;
+    const { originalURL, Password, expiryDate } = req.body;
+    
 
 
     if (!originalURL) {
         return res.status(400).json({ msg: "Enter URL first " })
     }
 
-
     let Shortcode;
     let RegisteredURL = true;
 
 
     while (RegisteredURL) {
-        Shortcode = generateRandomId();
-       RegisteredURL = await LInksData.findOne({ randomId: Shortcode });
+        Shortcode = nanoid(7); 
+       RegisteredURL = await LinksData.findOne({ randomId: Shortcode });
     }
 
-    const hashedPassword = password ?  await bcrypt.hash(password, 10) : null;
-    const userId = "67f123abc456def789";
+    const hashedPassword = Password ?  await bcrypt.hash(Password, 10) : null;
+  //   const userId = "67f123abc456def789"; temporary user id for testing 
+
+
 
     const newLInks = await LinksData.create({
-        usedBy: userId,
+        usedBy: new mongoose.Types.ObjectId("507f1f77bcf86cd799439011"),
         originalLink: originalURL,
         randomId: Shortcode,
         password:  hashedPassword,
         ExpiryDate: expiryDate ? new Date(expiryDate) : null,
         status: "ACTIVE"
     })
+
+    newLInks.save() ;
+
+
 
     res.status(201).json({
         ShortURL: `http://localhost:5000/${Shortcode}`
