024f27c1f24f7dd9553cf67c14d78658ce93188a
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-04-14 01:09:03 +0530

    Reduce max requests in redirect limiter to 100

--- Backend/middlewares/rateLimiter.js
+++ Backend/middlewares/rateLimiter.js
@@ -23,7 +23,7 @@ const loginLimiter = rateLimit({
 // 🔗 Redirect limiter (IMPORTANT)
 const redirectLimiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 10000000,
+  max: 100,
   msg: "Too many requests, slow down"
 });
 
