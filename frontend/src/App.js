f5940821618c92371f8ca8101e89436c6c2725de
Chandan koranga <chandan.koranga.2025@outlook.com>
2026-05-28 19:40:28 +0530

    Google Authentication options feature added in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,48 @@
-
-import './App.css';
-import HomePage from './Components/HomePage';
-import Error404 from './Components/Error404';
-import SignUp from './Components/SignUp';
-import Login from './Components/Login';
+import "./App.css";
+import HomePage from "./Components/HomePage";
+import Error404 from "./Components/Error404";
+import SignUp from "./Components/SignUp";
+import Login from "./Components/Login";
 import { Routes, Route } from "react-router-dom";
-import Redirecterror from './Components/Redirecterror'
-import Passwordredirect from './Components/Passwordredirect'
-import Contact from './Components/contact'
+import Redirecterror from "./Components/Redirecterror";
+import Passwordredirect from "./Components/Passwordredirect";
+import Contact from "./Components/contact";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+
+
 
 function App() {
-  
+
+    const GoogleAuthWrapperLogin = () => {
+    return (
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <Login />
+      </GoogleOAuthProvider>
+    );
+  };
+
+
+  const GoogleAuthWrapperSignup =() => {
+    return (
+      <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}>
+        <SignUp />
+      </GoogleOAuthProvider>
+    );
+  };
+
+
   return (
     <>
-    
-
-    <Routes>
-
-     <Route path="/contact" element={<Contact/>} />
-     <Route path="/" element={<HomePage/>}/>
-     <Route path="/login" element={<Login/>}/>
-     <Route path="/signup" element={<SignUp/>}/>
-     <Route path="*" element={<Error404/>}/>
-     <Route path='/verify/:ShortCode' element={<Passwordredirect/>}/>
-     <Route path="/error" element={<Redirecterror />} />
-    </Routes>
-    
-    
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<GoogleAuthWrapperLogin/>} />
+        <Route path="/signup" element={<GoogleAuthWrapperSignup/>} />
+        <Route path="*" element={<Error404 />} />
+        <Route path="/verify/:ShortCode" element={<Passwordredirect />} />
+        <Route path="/error" element={<Redirecterror />} />
+      </Routes>
     </>
   );
 }
